c80ba4e372b876e3ad537ef1e133c979eb841a44
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-11-08 08:48:35 +0100

    Creo screens, card, home y alert y colocamos las routes en el main y los import en "screen.dart"

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 
 class Listview2Screen extends StatelessWidget {
 
-  final options = const['Targaryen', 'Stark', 'Lanister', 'Baratheon'];
+  final options = const['Targaryen', 'Stark', 'Lannister', 'Baratheon'];
    
   const Listview2Screen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('ListView tipo 2'),
+        title: const Text('Listview Tipo 2'),
+        backgroundColor: Colors.blueAccent,
       ),
-      body: ListView.separated(itemBuilder:(context, index) => ListTile(
-            trailing: Icon(Icons.arrow_forward_ios_outlined),
-            title:Text(options[index]),
-          ), separatorBuilder:(context, index) => const Divider(), itemCount: options.length)
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+              trailing: Icon( Icons.arrow_forward_ios_outlined),
+              title: Text(options[index]),
+            ), 
+        separatorBuilder:(context, index) => const Divider(), 
+        itemCount: options.length
+      )
     );
   }
 }
