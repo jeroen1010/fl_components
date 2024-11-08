dbcd79eb806e27f2fb765cb31e8e7938a4cb73ce
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-11-08 09:53:31 +0100

    Creacion de menu option y models

---
+++ lib/models/menu_option.dart
@@ -0,0 +1,10 @@
+import 'package:fl_components/screens/screens.dart';
+
+class MenuOption {
+  final String route;
+  final IconData icon;
+  final String name;
+  final Widget screen;
+
+  MenuOption({required this.route, required this.icon, required this.name, required this.screen});
+}
