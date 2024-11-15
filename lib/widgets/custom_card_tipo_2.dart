4c6ec2d541b696752ced06c46f415c063bba7681
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2024-11-15 09:16:24 +0100

    Darle forma a los imagenes, container con el texto, GIF imagen de carga y borderRadius

--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -5,11 +5,25 @@ class CustomCardTipo2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Card(
+    return Card(
+      clipBehavior: Clip.antiAlias, //Cuando lago se sale del contenedor se corta (los bordes de la imagen se redondean)
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15) //Redondea el borde
+      ),
       child: Column(
         children: [
-          Image(
-            image: NetworkImage('https://wallpapercat.com/w/full/a/2/9/76539-2560x1442-desktop-hd-anuel-aa-background-image.jpg')
+          const FadeInImage(
+            image: NetworkImage('https://wallpapercat.com/w/full/a/2/9/76539-2560x1442-desktop-hd-anuel-aa-background-image.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 200,
+            //fit: BoxFit.cover, //Se adapta a todo el tamaño de widget
+            fadeInDuration: Duration(milliseconds: 3000),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(top:10, bottom: 10, right: 20),
+            child: const Text('Anuel AA')
           )
         ]
       )
